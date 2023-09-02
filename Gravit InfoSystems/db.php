35ad543223fb987be5f610cdc84d9b9069d7eb12
Tegor International <104412526+Tegorinternational@users.noreply.github.com>
2023-09-02 16:33:13 +0530

    Delete Gravit InfoSystems directory

--- Gravit InfoSystems/db.php
+++
@@ -1,3 +0,0 @@
-<?php  
-$db = mysqli_connect( '0.0.0.0:3306', 'root', 'root', 'gravit') or die('database is not connected');
-?>  
--- Gravit InfoSystems/includes/nav.php
+++
@@ -1,9 +0,0 @@
-<nav class="py-3 px-4 shadow">
-  <div class="flex flex-row">
-    <!-- logo -->
-    <h2 class="flex flex-col"><span class="text-left text-2xl font-medium tracking-tight my-auto"><?=$pageTitle ?></span>
-    <span class="text-xs"><?=$user_data['Name']?></span></h2>
-    <!-- sign in/ sign up button -->
-    <button onclick="window.location.href='<?=$actionUrl ?>'" class="ml-auto py-2 px-4 border border-black rounded-full my-auto text-sm"><?=$actionName ?> &#10132;</button>
-  </div>
-</nav>
